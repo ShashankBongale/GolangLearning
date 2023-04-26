0d25a412421789ee6d36473f0ded3277264d267b
shashankbongale <shashankbongale@gmail.com>
2023-04-26 21:53:20 +0530

    added loops

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 
 	//declaring and using implicit datatype
 	//Basic syntax with declaring any variable => var variableName dataType. var is a keyword which is used to declare one or more variables
-	var iVal int = 10
+	/*var iVal int = 10
 	iVal2 := 20
 	fmt.Println(iVal, iVal2)
 
 	//declaring and using array
 	var arr [3]int = [3]int{10, 25, 35}
 	fmt.Println(arr)
+
+	//declaration with initialisation
+	var arr3 [10]int
+	arr3[1] = 10
+	fmt.Println(arr3)
 
 	//declaring and using slice
 	var mySlice []int = []int{-100, -200}
@@ -58,6 +61,31 @@ func main() {
 
 	var myVar myType
 	myVar.name = "Random"
-	fmt.Println(myVar)
+	fmt.Println(myVar)*/
+
+	//working with loops
+	//var myArr [5]int = [5]int{10, 20, 30, 40, 50}
+
+	//infinite loop
+	/*for {
+		myArr[0] = 100
+		fmt.Println("DO NOT RUN THIS CODE AS THIS IS A INFINITE LOOP!!!!!!!")
+	}*/
+
+	iVal := 10
+
+	//this is like a while loop
+	for iVal <= 20 {
+
+		iVal += 5
+	}
+
+	fmt.Println("iVal ", iVal)
+
+	for i := 0; i < 10; i++ {
+		iVal += 1
+	}
+
+	fmt.Println("iVal ", iVal)
 
 }
