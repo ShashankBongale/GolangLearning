aaad225ddd3ee64739f73557a3d54a617caa719b
shashankbongale <shashankbongale@gmail.com>
2023-04-27 11:16:16 +0530

    added panic and recovery menthods

--- pkg2/source1.go
+++ pkg2/source1.go
@@ -3,9 +3,20 @@ package pkg2
 import "fmt"
 
 func MyPrint() {
-	defer fmt.Println("This is a defered statement")
+
+	defer func() {
+		msg := recover()
+		if msg != nil {
+			fmt.Println("Recovering from panic, error: ", msg)
+		}
+	}()
+
 	fmt.Println("This is my print from package2")
-	//panic("Something bad happened")
+
+	dividend := 0
+	iVal := 10 / dividend
+
+	fmt.Println("Result of division ", iVal)
 }
 
 var Pkg2Arr [3]int = [3]int{10, 20, 30}
